78ff6de03affa7d4a17b59db1fed1c0854596bb5
Balram Shukla <balramshukla003@gmail.com>
2024-08-10 22:29:33 +0530

    Update SelectionSort.java

--- Selection Sorting/SelectionSort.java
+++ Selection Sorting/SelectionSort.java
@@ -1,14 +1,20 @@
 class sorting {
     public int[] selectionSorting(int[] arr, int n) {
+
         int[] Arr = arr;
+
         for (int i = 0; i < n; i++) {
+            int minIndex = i;
             for (int j = i + 1; j < n; j++) {
-                if (Arr[i] > Arr[j]) {
-                    int temp = Arr[i];
-                    Arr[i] = Arr[j];
-                    Arr[j] = temp;
+
+                if (Arr[j] > Arr[minIndex]) {
+                    minIndex = j;
                 }
             }
+            int temp = Arr[minIndex];
+            Arr[minIndex] = Arr[i];
+            Arr[i] = temp;
+
         }
         return Arr;
     }
@@ -16,13 +22,13 @@ class sorting {
 public class selectionSort {
     public static void main(String[] args) {
 
-        int[] arr = {9, 8, 7, 6, 5, 4, 3};
+        int[] arr = {9, 8, 7, 11, 5, 15, 10};
         int n = 7;
 
         sorting s = new sorting();
         int[] k = s.selectionSorting(arr, n);
         for (int i = 0; i < n; i++) {
-            System.out.print(k[i]);
+            System.out.print(k[i]+" ");
         }
     }
 }
